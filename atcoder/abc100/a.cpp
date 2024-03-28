03e4d634f3e6ae1dd7be67c7cf6ea56a0c9411f0
whocarlos <149902458+whocarlos@users.noreply.github.com>
2024-03-28 16:11:27 -0600

    Create a.cpp

---
+++ atcoder/abc100/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if(a > 8 || b > 8){
+        cout << ":(" << endl;
+    }else{
+        cout << "Yay!" << endl;
+    }
+    
+    return 0;
+}
