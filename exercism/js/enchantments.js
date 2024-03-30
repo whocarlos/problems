035e4e9c7dff862caf837643b4e57790cc657187
whocarlos <149902458+whocarlos@users.noreply.github.com>
2024-03-29 18:26:07 -0600

    Create enchantments.js
    gotta keep the streak
    But what for?
    Front squats

---
+++ exercism/js/enchantments.js
@@ -0,0 +1,37 @@
+// @ts-check
+
+/**
+ * Determine how many cards of a certain type there are in the deck
+ *
+ * @param {number[]} stack
+ * @param {number} card
+ *
+ * @returns {number} number of cards of a single type there are in the deck
+ */
+export function cardTypeCheck(stack, card) {
+  let counter = 0;
+  stack.forEach((c) => {
+    if(card === c){
+      counter += 1;
+    }
+  });
+  return counter;
+}
+
+/**
+ * Determine how many cards are odd or even
+ *
+ * @param {number[]} stack
+ * @param {boolean} type the type of value to check for - odd or even
+ * @returns {number} number of cards that are either odd or even (depending on `type`)
+ */
+export function determineOddEvenCards(stack, type) {
+  let counter = 0;
+  for(let c of stack){
+    if(!(c % 2) == type){
+      counter += 1;
+    }
+  }
+
+  return counter;
+}
