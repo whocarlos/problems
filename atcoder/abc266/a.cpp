6590994e0d460f8aef6e76c6ec3df254b88689f1
whocarlos <149902458+whocarlos@users.noreply.github.com>
2024-04-21 13:23:12 -0600

    Create a.cpp
    ac

---
+++ atcoder/abc266/a.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    int i = s.size()/2;
+
+    cout << s[i] << endl;
+
+    return 0;
+}
