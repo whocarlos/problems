5ed82378bd79c3529439756ef6a7c03151182bb3
whocarlos <149902458+whocarlos@users.noreply.github.com>
2024-04-24 16:07:40 -0600

    Create a.cpp
    10 years or  6 months?

---
+++ atcoder/a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    string s;
+    
+    cin >> s;
+
+
+    if(s[n-1] == 'o') {
+        cout << "Yes" << endl;
+        return 0;
+    }
+
+    cout << "No" << endl;
+    return 0;
+}
