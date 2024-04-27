b6e2fec109769caa2de8c56c45e2234c2936be7f
whocarlos <149902458+whocarlos@users.noreply.github.com>
2024-04-26 18:09:57 -0600

    Create a.cpp
    I couldn't do a c after like 2 hours so... this

---
+++ atcoder/abc329/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <cmath>
+#include <string>
+
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    for(char& c : s){
+        cout << c << " ";
+    }
+    
+    cout << endl;
+    return 0;
+}
