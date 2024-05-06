def957ae76ce2605dea378c1d36554718f070603
whocarlos <149902458+whocarlos@users.noreply.github.com>
2024-05-06 12:23:11 -0600

    Create camelCase.cpp
    productive leg day in the AM

---
+++ hackerrank/alogorithms/strings/camelCase.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the 'camelcase' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts STRING s as parameter.
+ */
+
+int camelcase(string s) {
+    int counter = 1;
+    for(char letter : s){
+        if(isupper(letter)){
+            counter += 1;
+        }
+    }
+    
+    return counter;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = camelcase(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
