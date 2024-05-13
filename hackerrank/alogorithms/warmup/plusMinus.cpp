b3aecbc5fc6f8be4d6f6320dea19f5b3bb0187a6
whocarlos <149902458+whocarlos@users.noreply.github.com>
2024-05-13 12:19:28 -0600

    Create plusMinus.cpp
    double

---
+++ hackerrank/alogorithms/warmup/plusMinus.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+void plusMinus(vector<int> arr) {
+    double c_z = 0, c_n = 0, c_p = 0;
+    for(int n : arr){
+        if(n == 0){
+            c_z += 1;
+        }
+        
+        if(n < 0){
+            c_n += 1;
+        }
+        
+        if(n > 0){
+            c_p += 1;
+        }
+    }
+    
+    double n = arr.size();
+    
+    double p = c_p / n;
+    double ne = c_n / n;
+    double z = c_z / n;
+    
+    cout << p << endl;
+    cout << ne << endl;
+    cout << z << endl;
+    
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    plusMinus(arr);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
